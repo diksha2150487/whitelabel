6842d5ce94a6d1b8391f47b7494c59f7433959fa
diksha2150487 <130853102+diksha2150487@users.noreply.github.com>
2023-05-13 11:02:28 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,50 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <div className="container1">
+        <h2>Types of Partnerships</h2>
+        <div className="box">
+          <div className="first1">
+            <h3>CLOSED WHITE LABEL</h3>
+            <h5>
+              When you choose us for a closed white label partnership, we remain
+              behind the scenes. You and your team interact with the clients.
+              Your client would not know that we exist, and we work on their
+              project. Kushel Digi bill you, and you can bill your client.
+            </h5>
+          </div>
+          <div className="first1">
+            <h3>OPEN WHITE LABEL</h3>
+            <h5>
+              Under open white label partnership, we act as a part of your
+              in-house team and your clients would know that we exist. You
+              manage the project, take care of the client communication, and
+              guide us. We bill you and you may bill your end clients.
+            </h5>
+          </div>
+          <div className="first1">
+            <h3>REFERRAL</h3>
+            <h5>
+              When we join our client's project, become their full-time
+              employees. We participate in all development meetings and we do
+              everything we can to actively support product development.
+            </h5>
+          </div>
+        </div>
+      </div>
+      {/* ******************************************************** */}
+      <div className="container2">
+        <h3>Appreciation From Clients</h3>
+        <div className="box1">
+          <div className="img">
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
